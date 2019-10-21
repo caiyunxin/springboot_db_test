db2c6866fa142f960c09daa4a18629089796c909
caiyunxin <21151351@qq.com>
2019-10-21 16:54:20 +0800

    启动脚本

--- src/main/webapp/bin/run.sh
+++ src/main/webapp/bin/run.sh
@@ -1,89 +1,139 @@
 #!/bin/bash
-# @Author: caiyunxin@pinglian.hk
-# @Date: 2019/10/17
-# Usage: sh run.sh start|stop
-#进入当前脚本的目录，$0:当前shell脚本的文件名，dirname $0:获取当前shell的路径
+
 cd $(dirname $0)
-#定义赋值变量BIN_DIR,$(pwd):当前所在全路径
 export BIN_DIR=$(pwd)
 cd $BIN_DIR
-cd..
+cd ..
 DEPLOY_DIR=`pwd`
+LOG_DIR=$DEPLOY_DIR/logs
 PROJECT_NAME="springBootDemo-0.0.1-SNAPSHOT"
-APPLICATION_JAR = ${DEPLOY_DIR}/${PROJECT_NAME}.jar
-
-echo "DEPLOY_DIR... :"$DEPLOY_DIR 
-if [ -f $APPLICATION_JAR ]; then
-	echo "APPLICATION_JAR... :"$APPLICATION_JAR 
-fi
-
-# loading system environment
+PROJECT_JAR=$DEPLOY_DIR"/"$PROJECT_NAME".jar"
+STDOUT_FILE=$LOG_DIR/springBootDemo.out
 if [ -f $BIN_DIR/java_env.sh ]; then
 	source $BIN_DIR/java_env.sh
+else
+	echo "failed cause not exist java environment file"
+	return
 fi
 
+if [ ! -e $PROJECT_JAR ]; then
+	echo "file not exost or filename is wrong"
+	exit 0;
+fi
 RUNJAVA="$JAVA_HOME/bin/java"
-#判断当前应用程序是否已经执行0：未执行，1：执行
-function is_process_exist() {
-    local pid=$(ps axuf | grep -Pi "${RUNJAVA}.*${DEPLOY_DIR}" | grep -v grep | awk '{print $2}')
-
-    if [ "$pid"x = x ]; then
-        return 0
-    else
-        return 1
-    fi
+EXEC_PID_FILE="$LOG_DIR/instance.pid"
+#程序是否运行，0：未运行，1：运行
+function is_process_exist(){
+	local pid=$(ps axuf | grep -Pi "${RUNJAVA}.*${DEPLOY_DIR}" | grep -v grep | awk '{print $2}')
+	if [ "$pid"x = x ]; then
+		return 0
+	else
+        	return 1
+    	fi
+}
+#文件重命名
+function filemove(){
+	if [ -f "$1" ]; then
+		mv $1 $1.`date +%Y%m%d.%H%M`
+	fi
 }
 
 function start_service(){
-	nohup ${RUNJAVA} -jar ${APPLICATION_JAR} &
+	filemove $STDOUT_FILE
+	nohup $RUNJAVA "-jar" $PROJECT_JAR >> $STDOUT_FILE 2>&1 &	
+	echo -n $! > $EXEC_PID_FILE
 }
 
-#启动条件判断
-function start() {
+function start(){
 	echo "Starting..."
+	local sleep_time=5;
+	local retry_time=0;
+	local TOTAL_RETRY_TIME=5;
+
 	is_process_exist
 	if [ $? -eq 1 ]; then
 		echo "already started!"
 		exit 0;
 	fi
-	start_service
 	
+	start_service
+	while (( $retry_time < $TOTAL_RETRY_TIME ))
+	do
+		sleep $sleep_time
+		is_process_exist
+		if [ $? -ne 1 ]; then
+			(( retry_time += 1 ))
+			echo -ne "."
+		else
+			break
+		fi	
+	done
+	echo
+	
+	if [ $retry_time -ge $TOTAL_RETRY_TIME ]; then
+		echo "Error: process stop, failed!"
+		exit 1
+	else
+		echo "Info: process start."
+	fi
+	#打印输出
 }
-
+#停止程序
 function stop_service(){
-	PROJECT_PID=$(ps axuf | grep -Pi "${RUNJAVA}.*${DEPLOY_DIR}" | grep -v grep | awk '{print $2}')
-	 kill -9 $PROJECT_PID
+	if [ -f "$EXEC_PID_FILE" ]; then
+		EXEC_PID=$(cat $EXEC_PID_FILE)
+	else
+		EXEC_PID=$(ps axuf | grep ${DEPLOY_DIR} | grep -v grep | awk '{print $2}')
+	fi
+	kill $EXEC_PID > /home/work/spring_test_cyx/logs/kills 2>&1
+	COUNT=`ps --no-heading -C java -f --width 2000 | grep "$DEPLOY_DIR" | awk '{print $2}' | wc -l`
+	if [ $COUNT -gt 0 ]; then
+		sleep 5
+		kill -9 $EXEC_PID > /home/work/spring_test_cyx/logs/kills 2>&1
+	fi
+	rm -f $EXEC_PID_FILE
+
 }
 
-#停止条件判断
 function stop(){
 	echo "Stoping..."
 	is_process_exist
 	if [ $? -eq 0 ]; then
 		echo "already stoped!"
-        return
-    fi
+		return
+	fi
 	stop_service
+	if [ $? -eq 0 ]; then
+		echo "Info: process stop!"
+	fi
 }
-#重启程序
+
+#
 function restart(){
-	local pid=$(ps axuf | grep -Pi "${RUNJAVA}.*${DEPLOY_DIR}" | grep -v grep | awk '{print $2}')
+	echo "Info: process restarting..."
+	local pid=$(ps axuf | grep ${DEPLOY_DIR} | grep -v grep | awk '{print $2}')
 	if [ "$pid"x != x ]; then
-		# thread dump
-		kill -9 $pid
+		stop_service
+		if [ $? -ne 0 ]; then
+			echo "Info: process restart Error!"
+			return
+		fi
 	fi
 	start_service
+	if [ $? -eq 0 ]; then
+		echo "Info: process restarted!"
+	fi
 }
 
 case $1 in
 start)
-    start;;
+	start;;
 stop)
-    stop;;
+	stop;;
 restart)
-    restart;;
+	restart;;
 *)
-    echo "Usage: sh $0 {start|stop|restart}"
-    exit 1
-    ;;
+	echo "Usage: sh $0 {start|stop|restart}"
+	exit 1
+	;;
 esac
